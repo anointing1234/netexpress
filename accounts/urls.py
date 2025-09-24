a2067f33976344d32d6f7bfb22ebca009c358d4b
anointing1234 <yakubudestiny9@gmail.com>
2025-09-24 15:47:54 +0100

    commted

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.views.static import serve 
+from django.shortcuts import redirect
 
+def redirect_home(request):
+    return redirect("/", permanent=True)
 
 
 urlpatterns = [ 
     path('',views.home_view,name='home'),
-    path('home/',views.home_view,name='home'),
+    path("home/", redirect_home),
     path('about_us/',views.about_us,name='about_us'),
     path('services/',views.services,name='services'),
     path('tracking/',views.tracking,name='tracking'),
